9ad8479090e44aff03d2c3e1255355e30920524e
kisho-stack <jorgesc1903@gmail.com>
2026-02-19 12:17:40 +0100

    prueba de aplicacion ok

--- src/main/java/com/cursopoo/examenfinal/buscador/util/TextUtilities.java
+++ src/main/java/com/cursopoo/examenfinal/buscador/util/TextUtilities.java
@@ -3,6 +3,7 @@ package com.cursopoo.examenfinal.buscador.util;
 public class TextUtilities {
 
     public static String procesarTexto(String texto){
+
         return texto.trim().toUpperCase();
     }
 }
