cb39b41c8c3f16ecc808304453f64bf295272f2a
MDVeinte <maurodesruelles@gmail.com>
2026-02-19 11:49:18 +0100

    marcha atras

--- src/main/java/com/cursopoo/examenfinal/controller/SearchLibrariesController.java
+++ src/main/java/com/cursopoo/examenfinal/controller/SearchLibrariesController.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -31,7 +30,7 @@ public class SearchLibrariesController {
     }
 
     @PostMapping
-    public String search(Model model, @RequestParam(name = "busqueda", required = false) String texto){
+    public String search(Model model, String texto){
         log.info("[search]");
         log.debug("[texto:{}",texto);
 
